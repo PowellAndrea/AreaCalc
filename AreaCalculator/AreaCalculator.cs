5086143f856dcc2f62c41f9345c040bf54ee65d3
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-04-24 15:14:29 -0700

    fixed methods

--- AreaCalculator/AreaCalculator.cs
+++ AreaCalculator/AreaCalculator.cs
@@ -4,45 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ * Check percision on return
+ */
+
 namespace Area_Calc_Lastname
 {
    static class AreaCalculator
    {
       internal const decimal PI = 3.14M;
 
-      #region Squares
-      public static double getSquareArea(int _base, int _height)
+      public static double Calculate(int _base, int _height)
       {
          return _base * _height;
       }
-
-      public static double GetSquareArea(string _base, string _height)
+      
+      #region Squares
+      public static double getSquareArea(int _base)
       {
-         return 0;
-         //if (Convert.ToInt32(_base) >= 1 && Convert.ToInt32(_height) >= 1)
-         //{
-         //   return getSquareArea(Convert.ToInt32(_base), Convert.ToInt32(_height));
-         //}
-         //else
-         //{
-         //   return -1;
-         //}       
+         return Calculate(_base,_base);
       }
+
+      public static double GetSquareArea(string _base)
+      {
+         if (!string.IsNullOrEmpty(_base) && Convert.ToInt16(_base) >-1 && Convert.ToInt16(_base) <11)
+         { return Calculate(Convert.ToInt16(_base), Convert.ToInt16(_base));
+         }
+         else { return -1; }
+      }
+
       #endregion
 
       public static double getRectangleArea(int _base, int _height)
       {
-         return getSquareArea(_base, _height);
+         return Calculate(_base, _height);
       }
 
       public static double getParallelogramArea(int _base, int _height)
       {
-         return getSquareArea(_base, _height);
+         return Calculate(_base, _height);
       }
 
       public static double getTriangleArea(int _base, int _height)
       {
-         return .5 * getSquareArea(_base, _height);
+         return .5 * Calculate(_base, _height);
       }
 
       public static double getCircleRadius(int _circumference)
